8fb65314c5549234540b8691a1c5377ebb27e1de
Bonsanto <xbonsx@gmail.com>
2015-06-02 18:06:58 -0430

    Added the real function

--- app.js
+++ app.js
@@ -1,3 +1,12 @@
-/**
- * Created by Bonsanto on 6/2/2015.
- */
+var http = require("request");
+
+var fun = function (n) {
+	http("https://api.genderize.io/?name=peter", function (error, response, body) {
+		if (!error && response.statusCode === 200) {
+			console.log(n, body);
+			fun(++n);
+		}
+	});
+};
+
+fun(0);
