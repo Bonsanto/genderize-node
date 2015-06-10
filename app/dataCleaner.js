8887b062ccec57e6036d94b8db417a7a55fb06e9
Bonsanto <xbonsx@gmail.com>
2015-06-09 22:52:14 -0430

    - Improved how the id numbers are get from the nacional.csv. - Created a http get request function to avoid hard concurrency.

--- app/dataCleaner.js
+++ app/dataCleaner.js
@@ -4,7 +4,7 @@ var http = require("request"), fs = require("fs"), cheerio = require("cheerio");
  * @csvInPath string that represents the path and name of the source data
  * @csvOutPath string that represents the path and name of the file where the data will be stored after being processed
  * */
-var csvInPath = "../data/in/nacional.csv", csvOutPath = "../data/out/step1-ci.csv";
+var csvInPath = "../data/out/step1-ci.csv", csvOutPath = "../data/out/step1-ci1.csv";
 /**
  *
  * @sourcePath string that shows the location of the .csv to analyze.
@@ -21,52 +21,79 @@ var writeAllCIs = function (sourcePath, targetPath, fixerFunctions, encode, sour
     if (sourceRowSeparator === void 0) { sourceRowSeparator = "\n"; }
     if (targetColumnSeparator === void 0) { targetColumnSeparator = ","; }
     if (targetRowSeparator === void 0) { targetRowSeparator = "\n"; }
-    var currentCi = 0;
+    var leftOvers = "";
     fs.createReadStream(sourcePath).on("data", function (personsBatch) {
-        var persons = personsBatch.toString().split(sourceRowSeparator);
-        var idPersons = persons.filter(function (personString) { return parseInt(personString.split(sourceColumnSeparator)[1]); }).map(function (person) { return person.split(sourceColumnSeparator)[1]; }).join(targetRowSeparator);
-        currentCi += persons.length;
-        fs.appendFileSync(targetPath, idPersons);
-        console.log(new Date(), "Citizens written ".concat(currentCi.toString()));
+        leftOvers = leftOvers.concat(personsBatch.toString().replace(/[^0-9|\n]/g, ""));
     }).on("open", function () {
         console.log("Started on", new Date());
     }).on("close", function () {
+        fs.appendFileSync(targetPath, leftOvers.slice(1, leftOvers.length), {
+            "encode": "UTF-8"
+        });
+        console.log("Registers: ", leftOvers.split(targetRowSeparator).length);
         console.log("Finished on", new Date());
     }).on("error", function (msg) {
         console.log("Error on", new Date(), "Details: " + msg);
     });
 };
-//step 1 get all the cis.
-//writeAllCIs(csvInPath, csvOutPath);
 var queryCompleteName = function (sourcePath, targetPath, endpoint, fixerFunctions, encode, sourceColumnSeparator, sourceRowSeparator, targetColumnSeparator, targetRowSeparator) {
     if (encode === void 0) { encode = "UTF-8"; }
     if (sourceColumnSeparator === void 0) { sourceColumnSeparator = ","; }
     if (sourceRowSeparator === void 0) { sourceRowSeparator = "\n"; }
     if (targetColumnSeparator === void 0) { targetColumnSeparator = ","; }
     if (targetRowSeparator === void 0) { targetRowSeparator = "\n"; }
-    var currentCi = 0;
-    fs.createReadStream(sourcePath).on("data", function (personsBatch) {
-        var ids = personsBatch.toString().split(sourceRowSeparator);
-        ids.forEach(function (ci) {
-            http("http://cne.gob.ve/web/registro_electoral/ce.php?nacionalidad=V&cedula=" + ci, function (error, response, body) {
-                if (body && !error) {
-                    var $ = cheerio.load(body);
-                    if ($("tr b").length > 3) {
-                        var line = citizen[0] + "," + $("tr b")[3].children[0].data + "\n";
-                        fs.appendFileSync(targetPath, line);
-                    }
-                }
-            });
-        });
-        currentCi += ids.length;
-        fs.appendFileSync(targetPath, idPersons);
-        console.log(new Date(), "Citizens written ".concat(currentCi.toString()));
+    var leftOvers = "";
+    fs.createReadStream(sourcePath).on("data", function (personCi) {
+        leftOvers = leftOvers.concat(personCi.toString());
     }).on("open", function () {
         console.log("Started on", new Date());
     }).on("close", function () {
         console.log("Finished on", new Date());
+        var a = leftOvers.split(/\n/g);
+        //console.log(a);
+        a = a.slice(1, a.length - 1);
+        console.log(a[0]);
+        console.log(a[a.length - 1]);
+        var execute = function (data) {
+            var oldData = data;
+            var ci = data.pop();
+            var newData = data;
+            console.log("Missing", newData.length);
+            http(endpoint.concat(ci), function (error, response, body) {
+                if (body && !error) {
+                    var $ = cheerio.load(body);
+                    if ($("tr b").length > 3) {
+                        var line = ci + targetColumnSeparator + $("tr b")[3].children[0].data.replace(/\s+/g, " ") + targetRowSeparator;
+                        fs.appendFileSync(targetPath, line, {
+                            "encode": "UTF-8"
+                        });
+                    }
+                    else {
+                        fs.appendFileSync("../data/out/removed.csv", ci + targetRowSeparator, {
+                            "encode": "UTF-8"
+                        });
+                    }
+                    execute(newData);
+                }
+                else if (error) {
+                    console.log("error" + ci + error);
+                    fs.appendFileSync("../data/out/notResponded.csv", ci + targetRowSeparator, {
+                        "encode": "UTF-8"
+                    });
+                    execute(oldData);
+                }
+            });
+        };
+        console.log("Total is", a.length);
+        execute(a);
     }).on("error", function (msg) {
         console.log("Error on", new Date(), "Details: " + msg);
     });
 };
+//step 1 get all the cis.
+//writeAllCIs(csvInPath, csvOutPath);
+//step 2 get all the names from the API.
+csvInPath = "../data/out/step1-ci1.csv";
+csvOutPath = "../data/out/step2-ci-completeName.csv";
+queryCompleteName(csvInPath, csvOutPath, "http://cne.gob.ve/web/registro_electoral/ce.php?nacionalidad=V&cedula=");
 //# sourceMappingURL=dataCleaner.js.map
--- app/dataCleaner.ts
+++ app/dataCleaner.ts
@@ -8,8 +8,8 @@ var http = require("request"),
  * @csvInPath string that represents the path and name of the source data
  * @csvOutPath string that represents the path and name of the file where the data will be stored after being processed
  * */
-var csvInPath:string = "../data/in/nacional.csv",
-	csvOutPath:string = "../data/out/step1-ci.csv";
+var csvInPath:string = "../data/out/step1-ci.csv",
+	csvOutPath:string = "../data/out/step1-ci1.csv";
 
 
 /**
@@ -30,34 +30,26 @@ var writeAllCIs = (sourcePath:string,
 				   sourceRowSeparator:string = "\n",
 				   targetColumnSeparator:string = ",",
 				   targetRowSeparator:string = "\n") => {
-	var currentCi = 0;
+	var leftOvers:string = "";
 
 	fs.createReadStream(sourcePath)
-		.on("data", (personsBatch:string) => {
-			var persons:Array<string> = personsBatch.toString().split(sourceRowSeparator);
-
-			var idPersons = persons
-				.filter((personString:string) => parseInt(personString.split(sourceColumnSeparator)[1]))
-				.map((person) => person.split(sourceColumnSeparator)[1])
-				.join(targetRowSeparator);
-
-			currentCi += persons.length;
-			fs.appendFileSync(targetPath, idPersons);
-			console.log(new Date(), "Citizens written ".concat(currentCi.toString()));
+		.on("data", (personsBatch) => {
+			leftOvers = leftOvers.concat(personsBatch.toString().replace(/[^0-9|\n]/g, ""));
 		})
 		.on("open", () => {
 			console.log("Started on", new Date());
 		})
 		.on("close", () => {
+			fs.appendFileSync(targetPath, leftOvers.slice(1, leftOvers.length), {
+				"encode": "UTF-8"
+			});
+			console.log("Registers: ", leftOvers.split(targetRowSeparator).length);
 			console.log("Finished on", new Date());
 		})
 		.on("error", (msg) => {
 			console.log("Error on", new Date(), "Details: " + msg);
 		});
 };
-
-//step 1 get all the cis.
-//writeAllCIs(csvInPath, csvOutPath);
 
 var queryCompleteName = (sourcePath:string,
 						 targetPath:string,
@@ -68,36 +60,65 @@ var queryCompleteName = (sourcePath:string,
 						 sourceRowSeparator:string = "\n",
 						 targetColumnSeparator:string = ",",
 						 targetRowSeparator:string = "\n") => {
-	var currentCi = 0;
+	var leftOvers = "";
 
 	fs.createReadStream(sourcePath)
-		.on("data", (personsBatch:string) => {
-			var ids:Array<string> = personsBatch.toString().split(sourceRowSeparator);
-
-			ids.forEach(ci => {
-				http("http://cne.gob.ve/web/registro_electoral/ce.php?nacionalidad=V&cedula=" + ci, (error, response, body) => {
-					if (body && !error) {
-						var $ = cheerio.load(body);
-
-						if ($("tr b").length > 3) {
-							var line = citizen[0] + "," + $("tr b")[3].children[0].data + "\n";
-							fs.appendFileSync(targetPath, line);
-						}
-					}
-				});
-			});
-
-			currentCi += ids.length;
-			fs.appendFileSync(targetPath, idPersons);
-			console.log(new Date(), "Citizens written ".concat(currentCi.toString()));
+		.on("data", (personCi) => {
+			leftOvers = leftOvers.concat(personCi.toString());
 		})
 		.on("open", () => {
 			console.log("Started on", new Date());
 		})
 		.on("close", () => {
 			console.log("Finished on", new Date());
+			var a = leftOvers.split(/\n/g);
+			//console.log(a);
+			a = a.slice(1, a.length - 1);
+			console.log(a[0]);
+			console.log(a[a.length - 1]);
+
+			var execute = (data:Array<string>) => {
+				var oldData = data;
+				var ci = data.pop();
+				var newData = data;
+
+				console.log("Missing", newData.length);
+				http(endpoint.concat(ci), (error, response, body) => {
+					if (body && !error) {
+						var $ = cheerio.load(body);
+						if ($("tr b").length > 3) {
+							var line = ci + targetColumnSeparator + $("tr b")[3].children[0].data.replace(/\s+/g, " ") + targetRowSeparator;
+							fs.appendFileSync(targetPath, line, {
+								"encode": "UTF-8"
+							});
+						} else {
+							fs.appendFileSync("../data/out/removed.csv", ci + targetRowSeparator, {
+								"encode": "UTF-8"
+							});
+						}
+						execute(newData)
+					} else if (error) {
+						console.log("error" + ci + error);
+						fs.appendFileSync("../data/out/notResponded.csv", ci + targetRowSeparator, {
+							"encode": "UTF-8"
+						});
+						execute(oldData);
+					}
+				});
+			};
+
+			console.log("Total is", a.length);
+			execute(a);
 		})
 		.on("error", (msg) => {
 			console.log("Error on", new Date(), "Details: " + msg);
 		});
 };
+
+//step 1 get all the cis.
+//writeAllCIs(csvInPath, csvOutPath);
+
+//step 2 get all the names from the API.
+csvInPath = "../data/out/step1-ci1.csv";
+csvOutPath = "../data/out/step2-ci-completeName.csv";
+queryCompleteName(csvInPath, csvOutPath, "http://cne.gob.ve/web/registro_electoral/ce.php?nacionalidad=V&cedula=");
